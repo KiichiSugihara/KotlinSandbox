ba5d8646eeb25d32a2e3e6c726c2e8b80f818cd9
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-07-07 22:18:37 +0900

    feat (hello):Add Hello Type

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,3 +1,20 @@
 fun main(){
-    println("Hello Kotlin")
+    //変数定義
+    var arg:Int=1
+    val cons:String="Number"
+    println(arg)
+    println(cons)
+    //文字列テンプレート
+    val textTemp= "文字列"
+    println("$textTemp テンプレート")
+    //型変換 toTypeで変換
+    val str ="1"
+    val intVal: Int =str.toInt()
+    println(2 + intVal)
+    // nullを?で許容
+    var nullOk: String?=null
+    println(nullOk)
+    // array
+    val arrayOfInt: Array<Int> = arrayOf(1,2,3,4,5)
+    for (i in arrayOfInt) print("$i,")
 }
